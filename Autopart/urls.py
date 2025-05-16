438654b3b9884090caf35e9ad6fc1fb9b8189f0e
oozcode <antonio.lankin@gmail.com>
2025-05-16 14:08:22 -0400

    Initial commit

---
+++ Autopart/urls.py
@@ -0,0 +1,15 @@
+# Autoparts/urls.py
+
+from django.urls import path
+from . import views  # Importar las vistas de la aplicación
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('carrito/', views.carrito, name='carrito'),
+    path('login/', views.login_view, name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('pago/', views.pago, name='pago'),
+    path('frenos/', views.frenos, name='frenos'),
+    path('electricidad/', views.electricidad, name='electricidad'),
+]
---
+++ Autopart/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Vista para la página de inicio
+def index(request):
+    return render(request, 'autopart/index.html')
+
+# Vista para la página del catálogo
+def catalogo(request):
+    return render(request, 'autopart/catalogo.html')
+
+# Vista para la página del carrito de compras
+def carrito(request):
+    return render(request, 'autopart/carrito.html')
+
+# Vista para la página de inicio de sesión
+def login_view(request):
+    return render(request, 'autopart/login.html')
+
+# Vista para la página de registro
+def registro(request):
+    return render(request, 'autopart/registro.html')
+
+# Vista para la página de pago
+def pago(request):
+    return render(request, 'autopart/pago.html')
+
+# Vista para la página de frenos y suspensión
+def frenos(request):
+    return render(request, 'autopart/frenos.html')
+
+# Vista para la página de electricidad y baterías
+def electricidad(request):
+    return render(request, 'autopart/electricidad.html')
