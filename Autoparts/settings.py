489c2cf2c2e7183af7d773866eda1d5abb90cec9
DonFernando25 <fe.arevaloa@duocuc.cl>
2025-06-02 20:31:12 -0400

    BD +productos

--- Autoparts/settings.py
+++ Autoparts/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [ BASE_DIR /"Autopart"/ "static" ]
 
 
 # Default primary key field type
