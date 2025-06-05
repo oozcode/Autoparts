7d26b7b4fecb7cc42a85d64e1793fb6642b5356c
Baisuke <bast.carcamo@duocuc.cl>
2025-06-04 22:05:43 -0400

    Permisos

--- Autopart/admin.py
+++ Autopart/admin.py
@@ -7,5 +7,9 @@ admin.site.register(Categoria)
 from django.contrib import admin
 from .models import Marca  # importa Marca desde tus modelos
 from .models import MarcaAuto
+from .models import Order
+from .models import OrderItem
 admin.site.register(Marca)
 admin.site.register(MarcaAuto)
+admin.site.register(Order)
+admin.site.register(OrderItem)
